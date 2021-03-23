d76fdff8198ba586b4c09ca4085231f0b8c250b6
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-03-23 16:12:51 -0300

    old version

--- files/discord-oauth2.js
+++ files/discord-oauth2.js
@@ -2,7 +2,6 @@ module.exports = function (app, errorPage, vars, data, firebaseWeb, webCfg) {
 
     // Prepare Optional Module
     const optinalRequire = require('@tinypudding/puddy-lib/get/module');
-    const isEmulator = optinalRequire('@tinypudding/firebase-lib/isEmulator');
 
     // Prepare Discord JS
     const Discord = optinalRequire('discord.js');
@@ -12,7 +11,7 @@ module.exports = function (app, errorPage, vars, data, firebaseWeb, webCfg) {
 
     // Discord Redirect
     let discord_redirect = 'http://' + data.localhost + data.discord.url.redirect;
-    if (typeof isEmulator !== "function" || !isEmulator()) {
+    if (!require('@tinypudding/firebase-lib/isEmulator')()) {
         discord_redirect = `https://${data.cfg.domain}${data.discord.url.redirect}`;
     }
 
